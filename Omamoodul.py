620dbe4d6232908c5f45840bc28c36afaca9a003
opilane <opilane@HP-CZC2349HT9>
2023-02-28 13:37:26 +0200

    g

--- Omamoodul.py
+++ Omamoodul.py
@@ -1,22 +1,9 @@
-def inimese(linnad, inimesed):
-    if linnad in linnad.keys():
-        return f"The population of {linnad} is {linnad[linnad]} people."
-    else:
-        return 'The city is not in the list.' 
-def show_inf(linnad):
-    sorted_tuple = sorted(linnad.items())
-    return sorted_tuple
-def most_pop(cities):
-    big = 0
-    town = ''
-    for city, pop in cities.items():
-        if pop > big:
-            big = pop
-            town = city
-    return f'{town} : {big}'
-def less(cities, num):
-    my_dic = {}
-    for city, pop in cities.items():
-        if pop < num:
-            my_dic[city] = pop
-    return my_dic
+def add_city_population(cities, population):
+    while True:
+        city = input("Sisestage linna nimi: ")
+        people = int(input("Sisestage linna rahvaarv: "))
+        cities.append(city)
+        population.append(people)
+        if input("Kas soovite lisada veel linna (jah/ei)? ").lower() != "jah":
+            break
+
--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,30 +1,26 @@
 from Omamoodul import*
 linnad = []
 inimesed= []
-while True:
-        linn = input("Sisetage nimi linnas:" )
-        inimes = int(input("sisetage inimesed sest linnas:" ))
-        linn.append(linnad)
-        inimes.append(inimesed)
-        väljuda=input("sisetage  "o" linnaandmete sisestamise lõpetamiseks \või jätkamiseks vajutage sisestusklahvi:")
-        if väljuda.lower() == "o":
-            break
-linnas_dict = dict(zip(linnad, inimesed))
-while True:
-    käsk = input("käsk:1-Uurige linna nime sisestades, kui palju elanikke selles on/ 2-tähestikuline linnade loetelu ja elanike arv/ 3-Sisestage elanike arv ja kuvage linna nimi, kus on lähim elanikke/ 4-Leidke linnad, kus on vähem kui n elanikku/ 5-otsi esimese tähe järgiю")
-    if käsk == "1":
-        linn = input("sisetage nimi linnas:")
-        print(inimesed(linn,linnas_dict ))
-    if käsk == "2":
-        print(show_inf(linnas_dict))
-    if käsk == "3":
-        print(most_pop(linnas_dict))
-    if käsk == "4":
-        number= int(input("Sisesta rahvaarv:"))
-        print(less(linnas_dict, number))
-    if käsk== "5":
-        täht=input("sisetage esimene täht:")
-        sõna= [sõna for sõna in linnad if sõna.startswith(täht)]
-        print(sõna)
-    if käsk == "o":
-        break
+add_city_population(linnad, inimesed)
+
+       
+while True:  
+        linnas_dict = dict(zip(linnad, inimesed))
+        while True:
+            käsk = input("käsk:1-Uurige linna nime sisestades, kui palju elanikke selles on/ 2-tähestikuline linnade loetelu ja elanike arv/ 3-Sisestage elanike arv ja kuvage linna nimi, kus on lähim elanikke/ 4-Leidke linnad, kus on vähem kui n elanikku/ 5-otsi esimese tähe järgiю")
+            if käsk == "1":
+                linn = input("sisetage nimi linnas:")
+            print(inimesed(linn,linnas_dict ))
+            if käsk == "2":
+                print(show_inf(linnas_dict))
+            if käsk == "3":
+             print(most_pop(linnas_dict))
+            if käsk == "4":
+                number= int(input("Sisesta rahvaarv:"))
+                print(less(linnas_dict, number))
+            if käsk== "5":
+                täht=input("sisetage esimene täht:")
+                sõna= [sõna for sõna in linnad if sõna.startswith(täht)]
+                print(sõna)
+            if käsk == "o":
+                break
